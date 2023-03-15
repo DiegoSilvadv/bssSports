677a9d658b675456f759b2aaccc6dff578bc4250
Diego Silva <DiegoSilvadv>
2023-03-15 19:22:12 -0300

    First commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/Header';
+import Home from './src/pages/Home'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="auto" /> 
+      <Header />
+     
+      <Home />
     </View>
   );
 }
